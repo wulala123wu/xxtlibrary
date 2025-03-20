f4367b4957d39eb5fe7620df47cfe99fbe2dadc0
wulala123wu <1105004820@qq.com>
2025-03-20 19:12:41 +0800

    main2.py

--- main2.py
+++ main2.py
@@ -12,7 +12,7 @@ get_current_dayofweek = lambda action: time.strftime("%A", time.localtime(time.t
 
 
 SLEEPTIME = 0 # 每次抢座的间隔
-ENDTIME = "18:01:00" # 根据学校的预约座位时间+1min即可
+ENDTIME = "19:01:00" # 根据学校的预约座位时间+1min即可
 
 ENABLE_SLIDER = False # 是否有滑块验证
 MAX_ATTEMPT = 5000 # 最大尝试次数
